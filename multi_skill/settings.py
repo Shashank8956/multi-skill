492990285145ca0c38ce2afca306636fa93c211b
Vikramaditya Thakur <vsthakur451@gmail.com>
2020-05-20 18:09:40 +0530

    updated with new apps

--- multi_skill/settings.py
+++ multi_skill/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'area',
+    'station',
+    'employee',
+    'question',
+    'result',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +81,11 @@ WSGI_APPLICATION = 'multi_skill.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'multi_skill_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
     }
 }
 
