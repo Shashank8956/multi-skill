ef1dbbd6bcaaefcc73b82f0bbb57bc02c313970a
Ashutosh Paliwal <ashutoshpaliwal42@gmail.com>
2020-06-15 14:37:43 +0530

    Added TestHeader and TestQuestions models and updated adminview/views/add_test_details.py
    Formatted code according to PEP8 for that changed variuos variable names etc.

--- adminview/models.py
+++ adminview/models.py
@@ -1,18 +1,56 @@
 from django.db import models
 
+
 # Create your models here.
 class Employee(models.Model):
-    emp_token = models.IntegerField(unique = True)
+    emp_token = models.IntegerField(unique=True)
     emp_name = models.CharField(max_length=200)
     gender = models.CharField(max_length=7)
     current_station = models.CharField(default="Some station", max_length=200)
     mobile = models.CharField(max_length=12)
     doj = models.DateField(auto_now_add=True)
     language_preference = models.CharField(default="English", max_length=200)
-    createdOn = models.DateField(auto_now_add=True)
-    createdBy = models.CharField(default="Some Admin", max_length=200)
-    isAdmin = models.BooleanField(default=True)
+    created_on = models.DateField(auto_now_add=True)
+    created_by = models.CharField(default="Some Admin", max_length=200)
+    is_admin = models.BooleanField(default=True)
     objects = models.Manager()
 
 
-#emp = Employee(emp_token = 12345, emp_name = 'Trial Admin', gender = 'M', current_station = 'Some weird station', mobile = '9876543210', language_preference = 'English', createdBy = 'Some stupid admin 1', isAdmin = True)
+class TestHeader(models.Model):
+    test_title = models.CharField(max_length=200)
+    test_station = models.CharField(max_length=20)
+    test_stage = models.CharField(max_length=20)
+    no_of_questions = models.IntegerField()
+    test_time = models.IntegerField()
+    max_marks = models.IntegerField()
+
+
+class TestQuestions(models.Model):
+    test_id = models.ForeignKey(TestHeader, on_delete=models.DO_NOTHING)
+    question_number = models.IntegerField()
+    question = models.CharField(max_length=500)
+    option_1 = models.CharField(max_length=100)
+    option_2 = models.CharField(max_length=100)
+    option_3 = models.CharField(max_length=100)
+    option_4 = models.CharField(max_length=100)
+
+
+"""
+{
+    "title": "How are you?",
+    "station": "Stage 2",
+    "stage": "Stage 2",
+    "questions": "77",
+    "time": "8587",
+    "marks": "88",
+    "Question Details": [
+        {
+            "q1": "Add details for this question",
+            "Op1": "Add Option 1",
+            "Op2": "Add Option 2",
+            "Op3": "Add Option 1",
+            "Op4": "Add Option 2"
+        }
+    ]
+}
+"""
--- adminview/views.py
+++ adminview/views.py
@@ -1,43 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
-from .models import Employee
+from .models import Employee, TestHeader, TestQuestions
 import json
+
 
 # Create your views here.
 
 def display_login_page(request):
     return render(request, 'login.html')
 
+
 def display_dashboard(request):
     return render(request, 'admin/dashboard.html')
+
 
 def display_employee_page(request):
     return render(request, 'admin/employee.html')
 
+
 def display_test_page(request):
     return render(request, 'admin/test.html')
+
 
 def display_test_details(request):
     return render(request, 'admin/testDetail.html')
 
+
 def get_employee(request, emp_token):
     if request.method == 'GET':
         try:
-            emp = Employee.objects.get(emp_token = emp_token)
+            emp = Employee.objects.get(emp_token=emp_token)
             dictionary = {'emp_token': emp.emp_token,
-                            'emp_name': emp.emp_name,
-                            'gender': emp.gender,
-                            'mobile': emp.mobile,
-                            'doj': str(emp.doj),
-                            'current_station': emp.current_station,
-                            'language_preference': emp.language_preference,
-                            'createOn': str(emp.createdOn),
-                            'createdBy': emp.createdBy,
-                            'isAdmin': str(emp.isAdmin)}
+                          'emp_name': emp.emp_name,
+                          'gender': emp.gender,
+                          'mobile': emp.mobile,
+                          'doj': str(emp.doj),
+                          'current_station': emp.current_station,
+                          'language_preference': emp.language_preference,
+                          'createOn': str(emp.created_on),
+                          'createdBy': emp.created_by,
+                          'isAdmin': str(emp.is_admin)}
 
             response = json.dumps([dictionary])
-
         except:
             response = json.dumps([{'Error': 'Could not get data!'}])
     return HttpResponse(response, content_type='text/json')
@@ -51,23 +56,23 @@ def add_employee(request):
         token = payload['emp_token']
         name = payload['emp_name']
         gender = payload['gender']
-        currentStation = payload['current_station']
-        mobileNo = payload['mobile']
-        dateOfJoining = payload['doj']
+        current_station = payload['current_station']
+        mobile_no = payload['mobile']
+        date_of_joining = payload['doj']
         language_pref = payload['language_preference']
-        creationDate = payload['createOn']
-        createdby = payload['createdBy']
-        isAdmin = True
-        emp = Employee(emp_token = token,
-                        emp_name = name,
-                        gender = gender,
-                        mobile = mobileNo,
-                        doj = dateOfJoining,
-                        current_station = currentStation,
-                        language_preference = language_pref,
-                        createOn = creationDate,
-                        createdBy = createdby,
-                        isAdmin = isAdmin)
+        creation_date = payload['createOn']
+        created_by = payload['createdBy']
+        is_admin = True
+        emp = Employee(emp_token=token,
+                       emp_name=name,
+                       gender=gender,
+                       mobile=mobile_no,
+                       doj=date_of_joining,
+                       current_station=current_station,
+                       language_preference=language_pref,
+                       create_on=creation_date,
+                       created_by=created_by,
+                       is_admin=is_admin)
         try:
             emp.save()
             response = json.dumps([{'Success': 'Employee added successfully!'}])
@@ -78,74 +83,129 @@ def add_employee(request):
 
 @csrf_exempt
 def add_emp_trial(request):
-    #response = json.dumps([{}])
-    #print(request.POST.get('new_token',''))
-    #print(request.POST.get('new_name',''))
-    #print(request.POST.get('new_gender',''))
-    #print(request.POST.get('new_contact',''))
-    #print(request.POST.get('new_station',''))
-    #print(request.POST.get('new_doj',''))
+    # response = json.dumps([{}])
+    # print(request.POST.get('new_token',''))
+    # print(request.POST.get('new_name',''))
+    # print(request.POST.get('new_gender',''))
+    # print(request.POST.get('new_contact',''))
+    # print(request.POST.get('new_station',''))
+    # print(request.POST.get('new_doj',''))
 
     if request.method == 'POST':
         token = request.POST.get('new_token')
         name = request.POST.get('new_name')
         gender = request.POST.get('new_gender')
-        currentStation = request.POST.get('new_station')
-        mobileNo = request.POST.get('new_contact')
-        #dateOfJoining = request.POST.get('doj')
+        current_station = request.POST.get('new_station')
+        mobile_no = request.POST.get('new_contact')
+        # dateOfJoining = request.POST.get('doj')
         language_pref = 'English'
-        createdby = 'Some Name 1'
-        isAdmin = True
+        created_by = 'Some Name 1'
+        is_admin = True
         print("Before emp: ", token)
-        emp = Employee(emp_token = token,
-                        emp_name = name,
-                        gender = gender,
-                        mobile = mobileNo,
-                        #doj = DateField(dateOfJoining),
-                        current_station = currentStation,
-                        language_preference = language_pref,
-                        createdBy = createdby,
-                        isAdmin = isAdmin)
+        emp = Employee(emp_token=token,
+                       emp_name=name,
+                       gender=gender,
+                       mobile=mobile_no,
+                       # doj = DateField(dateOfJoining),
+                       current_station=current_station,
+                       language_preference=language_pref,
+                       created_by=created_by,
+                       is_admin=is_admin)
         try:
             print("Before save")
             emp.save()
             print("after save")
-            #response = json.dumps([{'Success': 'Employee added successfully!'}])
+            # response = json.dumps([{'Success': 'Employee added successfully!'}])
         except:
             response = json.dumps([{'Error': 'Employee could not be added!'}])
     return HttpResponseRedirect('/adminview/employee')
 
+
 # TO BE EDITED
+"""
+{
+    "title": "How are you?",
+    "station": "Stage 2",
+    "stage": "Stage 2",
+    "questions": "77",
+    "time": "8587",
+    "marks": "88",
+    "Question Details": [
+        {
+            "q1": "Add details for this question",
+            "Op1": "Add Option 1",
+            "Op2": "Add Option 2",
+            "Op3": "Add Option 1",
+            "Op4": "Add Option 2"
+        }
+    ]
+}
+"""
+
 
 def add_test_details(request):
     if request.method == 'POST':
         payload = json.loads(request.body)
-        print(payload['title'])
-        """
-       {"title":"How are you?","station":"Stage 1","stage":"Stage 1","questions":"545653165","time":"2512435","marks":"553365546","Question Details":[{"q1":"How the fuck you?","Op1":"Good Nigga","Op2":"Fynnn","Op3":"Good Nigga","Op4":"Fynnn"}]} 
-    """
+        print(json.dumps(payload, indent=4))
+        test_title = payload['title']
+        test_station = payload['station']
+        test_stage = payload['stage']
+        no_of_questions = payload['questions']
+        test_time = payload['time']
+        max_marks = payload['marks']
+        test_header = TestHeader(
+            test_title=test_title,
+            test_station=test_station,
+            test_stage=test_stage,
+            no_of_questions=no_of_questions,
+            test_time=test_time,
+            max_marks=max_marks
+        )
+        test_header.save()
+        test_id = test_header.id
+        print("TEST ID: ", test_id)
+        for question_details in payload['Question Details']:
+            question_number = question_details['Question Number']
+            question = question_details['Question']
+            option_1 = question_details['Op1']
+            option_2 = question_details['Op2']
+            option_3 = question_details['Op3']
+            option_4 = question_details['Op4']
+            test_question = TestQuestions(
+                test_id=test_id,
+                question_number=question_number,
+                question=question,
+                option_1=option_1,
+                option_2=option_2,
+                option_3=option_3,
+                option_4=option_4
+            )
+            test_question.save()
+            print("TEST QUESTION ID:", test_question.id)
+
     return HttpResponseRedirect('/adminview/test')
+
 
 def get_all_employees(request):
     if request.method == 'GET':
         try:
             emp = Employee.objects.all()
             print(emp)
-            dataArray = []
+            data_array = []
             for eachRow in emp:
                 dictionary = {'emp_token': eachRow.emp_token,
-                                'emp_name': eachRow.emp_name,
-                                'gender': eachRow.gender,
-                                'mobile': eachRow.mobile,
-                                'doj': str(eachRow.doj),
-                                'current_station': eachRow.current_station,
-                                'language_preference': eachRow.language_preference,
-                                'createOn': str(eachRow.createdOn),
-                                'createdBy': eachRow.createdBy,
-                                'isAdmin': str(eachRow.isAdmin)}
-                dataArray.append(dictionary)
-            print(dataArray)
-            response = json.dumps(dataArray)
+                              'emp_name': eachRow.emp_name,
+                              'gender': eachRow.gender,
+                              'mobile': eachRow.mobile,
+                              'doj': str(eachRow.doj),
+                              'current_station': eachRow.current_station,
+                              'language_preference': eachRow.language_preference,
+                              'createOn': str(eachRow.created_on),
+                              'createdBy': eachRow.created_by,
+                              'isAdmin': str(eachRow.is_admin)}
+                data_array.append(dictionary)
+            print(data_array)
+            response = json.dumps(data_array)
 
         except:
             response = json.dumps([{'Error': 'Could not get data!'}])
