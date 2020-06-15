fe4859d948f21b57ddbfb83fc749401c49315573
Vikramaditya Singh Thakur <singh.vikramsingh.vikram92@gmail.com>
2020-06-15 16:49:04 +0530

    deleted unnecessary apps

--- station/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Station
-
-class StationAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'Current_Manpower', 'Required_Manpower')
-    list_link = ('Name')
-
-admin.site.register(Station, StationAdmin)
