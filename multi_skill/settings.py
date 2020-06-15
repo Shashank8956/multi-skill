ea597f12c7e00407347fba1b8132de5a4e1321e7
Vikramaditya Singh Thakur <singh.vikramsingh.vikram92@gmail.com>
2020-06-15 17:10:04 +0530

    removed unnecessary app from INSTALLED_APPS

--- multi_skill/settings.py
+++ multi_skill/settings.py
@@ -40,12 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'adminview.apps.AdminviewConfig',
-
-    'station',
-    'stage',
-    'employee',
-    'question',
-    'result',
 ]
 
 MIDDLEWARE = [
