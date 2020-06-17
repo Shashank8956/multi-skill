6e7b21489c2e6cae018e33a58d8e436f3426935d
Vikramaditya Singh Thakur <singh.vikramsingh.vikram92@gmail.com>
2020-06-17 14:06:41 +0530

    refactored code based on pep8

--- adminview/models.py
+++ adminview/models.py
@@ -5,94 +5,99 @@ from django.contrib.auth.models import User
 from datetime import datetime
 
 class Station(models.Model):
-    Name = models.CharField(max_length = 50)
-    Current_Manpower = models.IntegerField(default = 0, blank = False)
-    Required_Manpower = models.IntegerField()
+    name = models.CharField(max_length = 50)
+    current_manpower = models.IntegerField(default = 0, blank = False)
+    required_manpower = models.IntegerField()
 
     def __str__(self):
-        return self.Name
+        return self.name
 
 
 class Stage(models.Model):
-    Name = models.CharField(max_length = 50)
-    Station = models.ForeignKey(Station, on_delete = models.DO_NOTHING)    
+    # Name = Skill, Novice = 1, Competent = 2, Proficient = 3, Expert = 4
+    name = models.CharField(max_length = 50) 
+    skill = models.IntegerField()   
 
     def __str__(self):
-        return self.Name
+        return self.name
 
 
 class Employee(models.Model):
-    Token = models.IntegerField()
-    Name = models.CharField(max_length = 50)
-    Gender = models.CharField(max_length = 50)
-    Contact = models.CharField(max_length = 50)
-    Email = models.CharField(max_length = 50, blank = True)
-    Station = models.ForeignKey(Station, on_delete = models.DO_NOTHING) 
-    Language_Preference = models.CharField(max_length = 50)
-    Date_Of_Joining = models.DateField()
-    Is_Admin = models.BooleanField(default=True)
-    Created_On = models.DateTimeField(default = datetime.now, blank = True)
-    Created_By = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+    token = models.IntegerField()
+    name = models.CharField(max_length = 50)
+    gender = models.CharField(max_length = 50)
+    contact = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 50, blank = True)
+    station = models.ForeignKey(Station, on_delete = models.DO_NOTHING) 
+    prefered_language = models.CharField(max_length = 50)
+    date_of_joining = models.DateField()
+    is_admin = models.BooleanField(default=True)
+    created_on = models.DateTimeField(default = datetime.now, blank = True)
+    created_by = models.ForeignKey(User, on_delete = models.DO_NOTHING)
 
     def __str__(self):
-        return self.Name
+        return self.name
 
-    #emp = Employee(emp_token = 12345, emp_name = 'Trial Admin', gender = 'M', current_station = 'Some weird station', mobile = '9876543210', language_preference = 'English', createdBy = 'Some stupid admin 1', isAdmin = True)
+    # emp = Employee(emp_token = 12345, emp_name = 'Trial Admin', gender = 'M', 
+    # current_station = 'Some weird station', mobile = '9876543210', 
+    # language_preference = 'English', createdBy = 'Some stupid admin 1', 
+    # isAdmin = True)
 
 
 class EmployeeSkill(models.Model):
-    Employee = models.ForeignKey(Employee, on_delete = models.DO_NOTHING)
-    Station = models.ForeignKey(Station, on_delete = models.DO_NOTHING)
-    Stage = models.ForeignKey(Stage, on_delete = models.DO_NOTHING)
-    Skill = models.IntegerField()
-    Test_Date = models.DateTimeField(default = datetime.now, blank = True)
+    employee = models.ForeignKey(Employee, on_delete = models.DO_NOTHING)
+    station = models.ForeignKey(Station, on_delete = models.DO_NOTHING)
+    stage = models.ForeignKey(Stage, on_delete = models.DO_NOTHING)   
+    test_date = models.DateTimeField(default = datetime.now, blank = True)
 
     def __str__(self):
-        return self.Employee.Name
+        return self.employee.name
 
 
 class TestHeader(models.Model):
-    Title = models.TextField()
-    Station = models.ForeignKey(Station, on_delete = models.DO_NOTHING)
-    Stage = models.ForeignKey(Stage, on_delete = models.DO_NOTHING)
-    Total_Question = models.IntegerField()
-    Passing_Percent = models.IntegerField()
-    Duration = models.IntegerField()
-    Created_On = models.DateTimeField(default = datetime.now, blank = True)
+    title = models.TextField()
+    station = models.ForeignKey(Station, on_delete = models.DO_NOTHING)
+    stage = models.ForeignKey(Stage, on_delete = models.DO_NOTHING)
+    total_question = models.IntegerField()
+    passing_marks = models.IntegerField()
+    duration = models.IntegerField()
+    created_on = models.DateTimeField(default = datetime.now, blank = True)
 
     def __str__(self):
-        return (str(self.Station) + " Test")
+        return (str(self.station) + "'s Test")
 
 
 class TestQuestion(models.Model):
-    Test = models.ForeignKey(TestHeader, on_delete = models.DO_NOTHING)
-    Question_Num = models.IntegerField()
-    Question = models.TextField()
-    Option_A = models.CharField(max_length = 50)
-    Option_B = models.CharField(max_length = 50)
-    Option_C = models.CharField(max_length = 50)
-    Option_D = models.CharField(max_length = 50)
-    Correct_Option = models.CharField(max_length = 1) #A, B, C, D
+    test = models.ForeignKey(TestHeader, on_delete = models.DO_NOTHING)
+    question_num = models.IntegerField()
+    question = models.TextField()
+    option_a = models.CharField(max_length = 50)
+    option_b = models.CharField(max_length = 50)
+    option_c = models.CharField(max_length = 50)
+    option_d = models.CharField(max_length = 50)
+    correct_option = models.CharField(max_length = 1) #A, B, C, D
 
     def __str__(self):
-        return (str(self.Test) + " Question " + str(self.Question_Num))
+        return (str(self.test) + "'s Question " + str(self.question_num))
 
 
 class ResultHeader(models.Model):
-    Test = models.ForeignKey(TestHeader, on_delete = models.DO_NOTHING)
-    Employee = models.ForeignKey(Employee, on_delete = models.DO_NOTHING)
-    Marks_Obtained = models.FloatField()
-    Total_Marks = models.FloatField()
-    Test_Date = models.DateField(default = datetime.now, blank = True)
+    test = models.ForeignKey(TestHeader, on_delete = models.DO_NOTHING)
+    employee = models.ForeignKey(Employee, on_delete = models.DO_NOTHING)
+    marks_obtained = models.FloatField()
+    total_marks = models.FloatField()
+    status = models.CharField(max_length = 4)
+    test_date = models.DateField(default = datetime.now, blank = True)
     
     def __str__(self):
-        return str(self.Employee.Name) + "'s " + str(self.Test.Area) + "'s Result"
+        return str(self.employee.name) + "'s " + str(self.test.station) + "'s Result"
 
 
 class ResultQuestion(models.Model):
-    Result = models.ForeignKey(ResultHeader, on_delete = models.DO_NOTHING)
-    Question = models.ForeignKey(TestQuestion, on_delete = models.DO_NOTHING)
-    Response = models.CharField(max_length = 1)
+    result = models.ForeignKey(ResultHeader, on_delete = models.DO_NOTHING)
+    question = models.ForeignKey(TestQuestion, on_delete = models.DO_NOTHING)
+    response = models.CharField(max_length = 1)
 
     def __str__(self):
-        return str(self.Result.Employee.Name) + "'s " + str(self.Result.Test.Area) + "'s Question" + str(self.Question.QuestionNum)
+        return str(self.result.employee.name) + "'s " + str(self.result.test.station)
+        + "'s Question" + str(self.question.question_num)
